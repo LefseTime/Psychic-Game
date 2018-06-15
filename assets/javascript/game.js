06ef415d7d78b6434bf64893b17d4516f7c3eda2
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-06-14 19:26:01 -0500

    added all letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //array of letters to draw from
-var letters = ["a", "b", "c", "d"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
 
 //generate random letter from array
 var letter = letters[Math.floor(Math.random() * letters.length)];
